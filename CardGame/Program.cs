6e48e64ee3a4efaf0669f8169b50070cc07471ad
Orion Weaver <awesomeboy7865@gmail.com>
2024-03-16 14:54:00 -0600

    Added CreateDeck feature method. Testing ShuffleDeck in dev at the moment.

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -15,14 +15,32 @@ namespace CardGame
 
     public abstract class Deck
     {
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
+
         public void CreateDeck()
         {
-
+            fullDeck.Clear();
+            
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)val });
+                }
+            }
         }
 
         public virtual void ShuffleDeck()
         {
-
+            /*
+             * Found this solution on Stack Overflow.
+             * Testing this solution in dev to see if it works.
+             */
+            
+            // var rand = new Random();
+            // var randomList = imagesEasy.OrderBy(x => rand.Next()).ToList();
         }
 
         public abstract List<PlayingCard> DealCard();
