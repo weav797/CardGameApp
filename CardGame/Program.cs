282cfd591f7b82d1c338d5a174d754a5a070bf63
Orion Weaver <awesomeboy7865@gmail.com>
2024-03-16 14:11:18 -0600

    Add project files.

---
+++ CardGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Deck
+    {
+        
+    }
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+}
