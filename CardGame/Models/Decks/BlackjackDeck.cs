7adadfb47a7d5da4e847d2415d877aa93cd6aa80
Orion Weaver <awesomeboy7865@gmail.com>
2024-03-20 11:49:17 -0600

    Full version of this mini-project. Ready for prod.

---
+++ CardGame/Models/Decks/BlackjackDeck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CardGame
+{
+    public class BlackjackDeck : Deck
+    {
+        public BlackjackDeck()
+        {
+            CreateDeck();
+            ShuffleDeck();
+        }
+
+        public override List<PlayingCardModel> DealCards()
+        {
+            List<PlayingCardModel> output = new List<PlayingCardModel>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                output.Add(DrawOneCard());
+            }
+
+            return output;
+        }
+
+        public PlayingCardModel RequestCard()
+        {
+            return DrawOneCard();
+        }
+    }
+
+    
+}
---
+++ CardGame/Models/Decks/PokerDeck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CardGame
+{
+    public class PokerDeck : Deck
+    {
+        public PokerDeck()
+        {
+            CreateDeck();
+            ShuffleDeck();
+        }
+
+        public override List<PlayingCardModel> DealCards()
+        {
+            List<PlayingCardModel> output = new List<PlayingCardModel>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                output.Add(DrawOneCard());
+            }
+
+            return output;
+        }
+
+        public List<PlayingCardModel> RequestCards(List<PlayingCardModel> cardsToDiscard)
+        {
+            List<PlayingCardModel> output = new List<PlayingCardModel>();
+
+            foreach (var card in cardsToDiscard)
+            {
+                output.Add(DrawOneCard());
+                discardPile.Add(card);
+            }
+
+            return output;
+        }
+    }
+
+    
+}
