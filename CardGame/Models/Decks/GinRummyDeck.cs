8219922814cac4565f36dd4aa111cae9f6ebce3c
Orion Weaver <awesomeboy7865@gmail.com>
2024-03-21 09:40:09 -0600

    GinRummyDeck class implemented. Ready for prod.

---
+++ CardGame/Models/Decks/GinRummyDeck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CardGame
+{
+    public class GinRummyDeck : Deck
+    {
+        public GinRummyDeck()
+        {
+            CreateDeck();
+            ShuffleDeck();
+        }
+
+        public override List<PlayingCardModel> DealCards()
+        {
+            List<PlayingCardModel> output = new List<PlayingCardModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                output.Add(DrawOneCard());
+            }
+
+            return output;
+        }
+
+        /* Can be improved later on since method repeats iteself. */
+        public List<PlayingCardModel> RequestCards(List<PlayingCardModel> cardsToDiscard)
+        {
+            List<PlayingCardModel> output = new List<PlayingCardModel>();
+
+            foreach (var card in cardsToDiscard)
+            {
+                output.Add(DrawOneCard());
+                discardPile.Add(card);
+            }
+
+            return output;
+        }
+    }
+}
